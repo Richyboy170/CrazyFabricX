7871243b4b14ea7344941bbad0732a7ec4acdbec
Richyboy170 <richyboy170@gmail.com>
2024-07-01 12:41:34 +0700

    1 June 2024/3

--- app/MyAccount/Points/page.tsx
+++ app/MyAccount/Points/page.tsx
@@ -15,7 +15,6 @@ const Points: React.FC = () => {
 
   useEffect(() => {
     // Fetch products and user points from API
-    // Example products
     const exampleProducts: Product[] = [
       { id: '1', image: '/images/sky.jpg', name: 'Sky T-Shirt', points: 100 },
       { id: '2', image: '/images/pink.jpg', name: 'Pink T-Shirt', points: 200 },
@@ -30,27 +29,25 @@ const Points: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6 text-center text-customGold">Exchange Your Points</h1>
-      <p className="mb-6 text-center text-lg text-white">You can exchange your points for the products below. Simply click on the product to confirm the exchange.</p>
+      <p className="mb-6 text-center text-lg text-customBlue">You can exchange your points for the products below. Simply click on the product to confirm the exchange.</p>
       <div className="grid grid-cols-1 gap-8">
         {products.map((product) => (
-          <div key={product.id} className="ticket-card">
-            <div className="ticket-content">
-              <div className="ticket-points text-customDarkBlue">{product.points} Points</div>
-              <div className="ticket-info">
+          <div key={product.id} className="ticket-card flex">
+            <div className="ticket-points bg-blue-500 text-white flex items-center justify-center p-4">
+              {product.points} Points
+            </div>
+            <div className="ticket-image flex-grow relative">
+              <Image src={product.image} alt={product.name} layout="fill" objectFit="cover" className="rounded" />
+              <div className="absolute bottom-0 left-0 p-4 text-white bg-black bg-opacity-50 w-full">
+                <h2 className="text-2xl font-bold">{product.name}</h2>
+                <p className="text-lg">Free</p>
                 <button
                   onClick={() => handleExchange(product.points)}
-                  className="bg-white text-customDarkGreen px-4 py-2 rounded"
+                  className="mt-4 bg-white text-blue-500 px-4 py-2 rounded"
                   disabled={userPoints < product.points}
                 >
                   Exchange
                 </button>
-              </div>
-            </div>
-            <div className="ticket-image">
-              <Image src={product.image} alt={product.name} layout="fill" objectFit="cover" className="rounded" />
-              <div className="absolute bottom-0 left-0 p-4 text-customDarkBlue">
-                <h2 className="text-2xl font-bold">{product.name}</h2>
-                <p className="text-lg">Free</p>
               </div>
             </div>
           </div>
